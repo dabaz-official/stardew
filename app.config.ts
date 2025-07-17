e6bde69ca93d84d1a21255ed73a71284b59ce555
DabAZ <dabaz@dabaz.me>
2025-07-18 04:02:09 +0800

    Setup project

---
+++ app.config.ts
@@ -0,0 +1,67 @@
+export default defineAppConfig({
+  shadcnDocs: {
+    site: {
+      name: 'stardew-valley',
+      description: '一个给星露谷玩家的在线文档',
+    },
+    theme: {
+      customizable: true,
+      color: 'zinc',
+      radius: 0.5,
+    },
+    header: {
+      title: '星露谷 Wiki by DabAZ',
+      showTitle: true,
+      darkModeToggle: true,
+      languageSwitcher: {
+        enable: false,
+        triggerType: 'icon',
+        dropdownType: 'select',
+      },
+      logo: {
+        light: '/logo.svg',
+        dark: '/logo-dark.svg',
+      },
+      nav: [],
+      links: [{
+        icon: 'lucide:github',
+        to: 'https://github.com/dabaz-official/stardew',
+        target: '_blank',
+      }],
+    },
+    aside: {
+      useLevel: true,
+      collapse: false,
+    },
+    main: {
+      breadCrumb: true,
+      showTitle: true,
+    },
+    footer: {
+      credits: 'Copyright © 2025',
+      links: [{
+        icon: 'lucide:github',
+        to: 'https://github.com/dabaz-official/stardew',
+        target: '_blank',
+      }],
+    },
+    toc: {
+      enable: true,
+      links: [{
+        title: 'Star on GitHub',
+        icon: 'lucide:star',
+        to: 'https://github.com/dabaz-official/stardew',
+        target: '_blank',
+      }, {
+        title: 'Create Issues',
+        icon: 'lucide:circle-dot',
+        to: 'https://github.com/dabaz-official/stardew/issues',
+        target: '_blank',
+      }],
+    },
+    search: {
+      enable: true,
+      inAside: false,
+    }
+  }
+});
---
+++ content/index.md
@@ -0,0 +1,26 @@
+---
+title: Home
+navigation: false
+---
+
+::hero
+---
+actions:
+  - name: Documentation
+    to: https://shadcn-docs-nuxt.vercel.app/getting-started/introduction
+  - name: GitHub
+    variant: outline
+    to: https://github.com/dabaz-official/stardew
+    leftIcon: 'lucide:github'
+---
+
+#title
+欢迎来到星露谷 Wiki
+
+#description
+探索**星露谷 Wiki**，你的《星露谷物语》中文攻略宝库！从 NPC 好感度到物品获取、任务指南再到农场规划，这里为你提供全面、详尽的游戏资讯，助你打造梦想中的田园生活！
+::
+
+::div{class="border rounded-lg shadow-md"}
+  :iframe{src="https://en.wikipedia.org/wiki/Stardew_Valley" height="700" width="100%" class="rounded-lg" scrolling="no" frameborder="0"}
+::
